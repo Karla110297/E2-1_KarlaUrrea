2fa23b3093d0dbf48eff10cf828da9893bc3af62
Karla110297 <dinorakarla@hotmail.com>
2018-09-27 23:46:03 -0700

    E2-1
    Karla Dinora Urrea Soto

---
+++ E2-1_KarlaUrrea/OperacionesFactoriales.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_1_KarlaUrrea
+{
+    class OperacionesFactoriales
+    {
+        public OperacionesFactoriales() { }
+
+      
+
+        public int Factorial(int n)
+        {
+            int nFactorial = 1;
+
+            if (n > -1 && n < 2)
+            {
+                nFactorial = 1;
+            }
+            else if (n < 0)
+            {
+                nFactorial= 0;
+            }
+            else
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    nFactorial *= i;
+                }
+
+            }
+
+            return nFactorial;
+        }
+
+        public int FactorialRecursividad(int n)
+        {
+            if (n > -1 && n < 2)
+            {
+                return 1;
+            }
+            else if (n < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n * FactorialRecursividad(n - 1);
+            }
+        }
+    }
+}
---
+++ E2-1_KarlaUrrea/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_1_KarlaUrrea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacionesFactoriales operacion = new OperacionesFactoriales();
+
+
+            int[] n = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Numero n para sacar factorial");
+                n[i] = Int32.Parse(Console.ReadLine());
+
+            }
+
+
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("{0}!= {1} ", n[i], operacion.FactorialRecursividad(n[i]));
+            }
+
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("Tiempo: " + stop.Subtract(start).TotalMilliseconds);
+
+
+            Console.WriteLine();
+
+
+            TimeSpan parar;
+            TimeSpan comenzar = new TimeSpan(DateTime.Now.Ticks);
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("{0}!= {1} ", n[i], operacion.Factorial(n[i]));
+            }
+
+            parar = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("Tiempo: " + parar.Subtract(comenzar).TotalMilliseconds);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
